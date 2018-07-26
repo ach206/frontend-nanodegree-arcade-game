2a1a91b8fdb12ae6d56aed840908690a42f49211
Camille Hall <camille@creativeambition.net>
2018-07-26 15:32:36 -0700

    player and 4 enemies displayed on canvas

--- js/engine.js
+++ js/engine.js
@@ -146,9 +146,9 @@ var Engine = (function(global) {
      * on your enemy and player entities within app.js
      */
     function renderEntities() {
-        /* Loop through all of the objects within the allEnemies array and call
-         * the render function you have defined.
-         */
+        //  Loop through all of the objects within the allEnemies array and call
+        //  * the render function you have defined.
+         
         allEnemies.forEach(function(enemy) {
             enemy.render();
         });
@@ -173,7 +173,8 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/char-pink-girl.png'
     ]);
     Resources.onReady(init);
 
