89ef32e50f19847614398ae5224358ae24e97e9e
Camille Hall <camille@creativeambition.net>
2018-07-26 19:46:55 -0700

    bugs move across continuously

--- js/app.js
+++ js/app.js
@@ -16,7 +16,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if (this.x > 505){
+        this.x = 0;
+        this.x = dt * (Math.random() * 101);
+    } else {
     this.x += dt * (Math.random() * this.x);
+    }
 };
  
 // Draw the enemy on the screen, required method for game
